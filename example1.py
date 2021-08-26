6a600c546474ae00209b7d7939c91ceebe42024c
jessered <carsten.klingner@gmail.com>
2021-08-26 21:00:37 +0200

    example1 file added

---
+++ example1.py
@@ -0,0 +1,36 @@
+def func1(arg1, arg2):
+    """This function tage two arguments , sets the first to eqal the ssevond, then return the new first argument. Pointless
+
+    Args:
+        arg1 ([type]): a value
+        arg2 ([type]): another value
+
+    Returns:
+        [type]: arg1
+    """
+
+    arg1 = arg2
+    return arg1
+
+
+def func2(arg1, arg2):
+    """[summary]
+
+    Args:
+        arg1 ([type]): [description]
+        arg2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    arg1 = arg2
+    return arg1
+
+
+def func3():
+    """function with no arguement returns None
+
+    Returns:
+        None: None
+    """
+    return None
